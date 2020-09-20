ab8884c48b15034a5027bbc459b616548a1bbe9d
Rowadz <38977667+MohammedAl-Rowad@users.noreply.github.com>
2020-09-20 22:06:51 +0300

    Update read.crud.js

--- db_stuff/CRUD/read.crud.js
+++ db_stuff/CRUD/read.crud.js
@@ -1,5 +1,9 @@
 const queries = async (db) => {
   const { users } = db.collections
+  const query = users.find()
+  query.$.subscribe(result => {
+    console.log(result.length) // the count of the users we have & this will be called when ever the users collection state gets updated
+  })
   const userDoc = await users
     .findOne()
     .where('name')
